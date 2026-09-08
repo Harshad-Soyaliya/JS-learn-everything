a7380a63e2671dad1c4cc2c57e44fc00fd17e56b
Harshad-Soyaliya <soyaliyaharshad96@gmail.com>
2026-09-09 00:06:19 +0530

    complete chapter 3

---
+++ Chapter 3: Operators /script.js
@@ -0,0 +1,22 @@
+/* 
+
+Arithmetic : +  -  *  /  %  ** //
+comparision : == === != !== >= <= > <
+Assignment : = += -= *= /= %=
+logical : && || !
+unary : + - ! typeof ++ --
+ternary : condition ? if true so : if false so
+
+
+typeof : for idetify types , this works for primitive
+
+
+instanceof : means a anu chhe evi rite , also it works only referencd value means non primitive
+ex : 
+
+let a = []
+
+here if typeof(a) -> object
+and if :- a instaceof Arrary -> true
+
+*/
